e490e0f25108ee48a466d6eafa3ca8b6f212f450
Dasha Grebenchuk <d.a.malevanchenko@gmail.com>
2022-08-26 17:53:34 +0200

    Added Pexels API for Photo section

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,30 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
     search();
   }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000012427f3ae28fe4e2287a2808c2c79b5b8";
+    let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleResponse(response) {
@@ -47,6 +58,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
